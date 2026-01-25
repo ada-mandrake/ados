7860a6b016d328cb057c7a3b9e6e6eb1757986ce
Winterer, Mathis Aaron <winterer.mathis.aaron@outlook.com>
2026-01-25 20:57:23 +0100

    Improve serial driver
    Signed-off-by: Winterer, Mathis Aaron <winterer.mathis.aaron@outlook.com>

--- src/kernel/drivers/serial/serial.adb
+++ src/kernel/drivers/serial/serial.adb
@@ -2,6 +2,37 @@ with System;
 with x86.Port_IO; use x86.Port_IO;
 
 package body Serial is
+
+   function Initialise_Serial (Port : Integer) return Integer is
+   begin
+      Outb (System'To_Address (Port + 1), 16#00#);
+      --  Disable all interrupts
+      Outb (System'To_Address (Port + 3), 16#80#);
+      --  Enable DLAB
+      Outb (System'To_Address (Port + 0), 16#03#);
+      --  Set divisor to 3 (lo byte) 38400 baud
+      Outb (System'To_Address (Port + 1), 16#00#);
+      --                  (hi byte)
+      Outb (System'To_Address (Port + 3), 16#03#);
+      --  8 bits, no parity, one stop bit
+      Outb (System'To_Address (Port + 2), 16#C7#);
+      --  Enable FIFO, clear them, with a 14-byte threshold
+      Outb (System'To_Address (Port + 4), 16#0B#);
+      --  IRQs enabled, RTS/DSR set
+      Outb (System'To_Address (Port + 4), 16#1E#);
+      --  Set in loopback mode, test the serial chip
+      Outb (System'To_Address (Port + 0), 16#AE#);
+      --  Test serial chip
+      --  (send byte 0xAE and check if serial returns the same byte)
+
+      if Inb (System'To_Address (Port)) /= 16#AE# then
+         return 1;
+      end if;
+
+      Outb (System'To_Address (Port + 4), 16#0F#);
+      return 0;
+   end Initialise_Serial;
+
    procedure Write_Char (Port : Unsigned_16; C : Character) is
    begin
       Outb (System'To_Address (Port), Character'Pos (C));
--- src/kernel/drivers/serial/serial.ads
+++ src/kernel/drivers/serial/serial.ads
@@ -2,7 +2,15 @@ with Interfaces; use Interfaces;
 
 package Serial is
    COM1 : constant := 16#3F8#;
+   COM2 : constant := 16#2F8#;
+   COM3 : constant := 16#3E8#;
+   COM4 : constant := 16#2E8#;
+   COM5 : constant := 16#5F8#;
+   COM6 : constant := 16#4F8#;
+   COM7 : constant := 16#5E8#;
+   COM8 : constant := 16#4E8#;
 
+   function Initialise_Serial (Port : Integer) return Integer;
    procedure Write_Char (Port : Unsigned_16; C : Character);
    procedure Write_String (Port : Unsigned_16; S : String);
 end Serial;
